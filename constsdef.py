86ccb573d136be23d806457fa7b9c1e4c9687efd
bboot9001 <wwwang9001@163.com>
2016-12-11 21:51:10 +0800

    删除隐私文件

--- constsdef.py
+++ constsdef.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-STNAME_DEV = 'localhost'
+HOSTNAME_DEV = 'localhost'
 DATABASE_DEV = 'pyblog_dev'
-USERNAME_DEV = 'root'
-PASSWORD_DEV = 'bboot9001wp'
+USERNAME_DEV = 'username'
+PASSWORD_DEV = 'password'
 DB_URI_DEV = 'mysql://{}:{}@{}/{}'.format(
     USERNAME_DEV, PASSWORD_DEV, HOSTNAME_DEV, DATABASE_DEV)
 
