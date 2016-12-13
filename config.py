ce32b5bdd5fa7c6a2108559f1dfbdf8fcdfaedad
bboot9001 <wwwang9001@163.com>
2016-12-13 23:57:12 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -12,6 +12,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class ConfigBase:
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[BLOG]'
+    FLASKY_MAIL_SENDER = 'Blog Admin <623633583@qq.com>'
+    FLASKY_ADMIN = os.environ.get('BLOG_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
+
 
     @staticmethod
     def init_app(app):
