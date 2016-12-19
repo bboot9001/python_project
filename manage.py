f697aa1574983eda8cc6db7bf35e9aa5bd773bd8
bboot9001 <wwwang9001@163.com>
2016-12-20 00:01:20 +0800

    "添加用户角色"

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 from flask_script import Manager, Shell, Option,Command
-from app.models import User, Role
+from app.models import User, Role, Permission
 from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 app = create_app('development')
@@ -10,7 +10,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
